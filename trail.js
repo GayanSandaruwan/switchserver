ec03f30b8864e3aea2e9aaeb5b723f7c944ba992
Gayan Sandaruwan <gayankavirathne.14@cse.mrt.ac.lk>
2017-10-24 10:30:14 +0530

    Change Of MONGOdb USER

--- trail.js
+++
@@ -1,8 +0,0 @@
-var query = {username':req.user.username};
-req.newData.username = req.user.username;
-
-
-MyModel.findOneAndUpdate(query, req.newData, {upsert:true}, function(err, doc){
-	    if (err) return res.send(500, { error: err });
-	    return res.send("succesfully saved");
-});
